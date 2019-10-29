ce28522c0d7e3c55115468e9d63e7dd935809f90
nhatndm <nhatndm1193@gmail.com>
2019-10-29 20:46:20 +0700

    init project, remove unused files

---
+++ src/Presentations/App/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./index.scss";
+
+export default class App extends Component {
+  render() {
+    return <div> this is seat selection</div>;
+  }
+}
